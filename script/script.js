c39ece84b921753a517f16df12f75f313defdaec
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-19 02:32:57 +0330

    add showError

--- script/script.js
+++ script/script.js
@@ -4,7 +4,14 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// show input error message
 
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
 
 // Event listeners
 
