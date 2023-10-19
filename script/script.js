1e4f5b89f85153d75f7f0eedcedec52355aacc72
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-19 20:08:20 +0330

    add js

--- script/script.js
+++ script/script.js
@@ -18,6 +18,16 @@ function showSuccess(input) {
   formControl.className = "form-control success ";
 }
 
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
 // Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
